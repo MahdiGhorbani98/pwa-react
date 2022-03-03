2eb63ffce3a72f3cf2e9963c1441e547bb585036
MahdiGhorbani <67688553+oliver19999@users.noreply.github.com>
2022-03-03 05:15:42 +0330

    name-country+cssStyle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
 import "./App.css";
+import { fetchWeather } from "./api/FetchWeather";
 
 function App() {
-  return <div className="App">App</div>;
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      setQuery("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search ..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="country">
+          <span>{weather.name}</span>
+          <sup>{weather.sys.country}</sup>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
